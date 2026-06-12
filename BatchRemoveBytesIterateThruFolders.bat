18110076abff3d6b3c3fa22d104c714ab7cc5fbd
GypsyFromTheSatelliteofLove <bogsbotchog@gmail.com>
2026-06-12 18:44:29 +0800

    Implement file size check before processing files
    Added a file size check to skip processing small files.

--- BatchRemoveBytesIterateThruFolders.bat
+++ BatchRemoveBytesIterateThruFolders.bat
@@ -3,11 +3,14 @@
 set "bytes_to_skip=6"
 
 ::cd /d "%target_dir%"
-echo Processing, this make take up to a few minutes...
+echo Processing, this may take up to a few minutes...
 
 for /d %%i in (*) do (
     echo %%i
-    for %%F in (%%i\*_Mek) do (
-        powershell -Command "$b = [System.IO.File]::ReadAllBytes('%%F'); if ($b.Length -gt %bytes_to_skip%) {                              [System.IO.File]::WriteAllBytes('%%F.shp', $b[%bytes_to_skip%..($b.Length-1)]) }"
+    set "fileSize=%%~zA"
+    if fileSize GTR 100 (
+        for %%F in (%%i\*_Mek) do (
+        powershell -Command "$b = [System.IO.File]::ReadAllBytes('%%F'); if ($b.Length -gt %bytes_to_skip%) {[System.IO.File]::WriteAllBytes('%%F.shp', $b[%bytes_to_skip%..($b.Length-1)]) }"
+        )
     )
 )
